0a9adaf079d3bde3bd60b3167a43964b70be691d
AngeOro <angelica-om@outlook.com>
2022-01-29 16:04:27 -0500

    Se resuelven conflictos

--- src/helpers/http-common.js
+++ src/helpers/http-common.js
@@ -1,16 +1,2 @@
-<<<<<<< HEAD
 export const url_api = `http://localhost:3000/`;
 export const url_api_localhost = `http://localhost:3000/`;
-
-// import axios from "axios";
-
-// export default axios.create({
-//   baseURL: "http://localhost:3000/",
-//   headers: {
-//     "Content-type": "application/json"
-//   }
-// });
-=======
-export const url_api = `https://app-sotransiv-ppi.herokuapp.com/`;
-export const url_api_localhost = `http://localhost:3000/`;
->>>>>>> b84660b8e65c7146ad57921be6ff11a5791e62b6
