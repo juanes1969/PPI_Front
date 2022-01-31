981f3283b9de7aff25e1b013bc52b029bf20e5e4
edwaralej12 <edwaralejandro595@gmail.com>
2022-01-31 18:01:35 -0500

    Se hace el buscador

--- src/components/conduct/SearchConduct.js
+++ src/components/conduct/SearchConduct.js
@@ -7,28 +7,29 @@ import '../../Styles/SearchConduct.css';
 import PropTypes from 'prop-types';
 
 
-export const SearchConduct = ({ titleButton, icon, openModal }) => {
 
+
+export const SearchConduct = ({ titleButton, icon, openModal}) => {
+    
+
+    
     return (
         <>
             <nav className="navbar navbar-light bg-light" id="navbarI">
                 <div className="container-fluid">
                     <div className="navbar-brand" id="buttonsActions">
-                        <button type="button" className="btn btn-success"><AiIcons.AiFillFileExcel /></button>
-                        <button type="button" className="btn btn-danger"><AiIcons.AiFillFilePdf /></button>
-                        <button type="button" className="btn btn-info"><ImIcons.ImPrinter /></button>
                         <button type="button" className="btn btn-warning" id="buttonAdd" onClick={openModal}>{icon} {titleButton}</button>
                     </div>
-                    <form className="d-flex navbar-brand" id="formSearch">
-                        <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit"><FcIcons.FcSearch /></button>
-                    </form>
+                   
                 </div>
-            </nav>            
+            </nav>
         </>
     )
 }
 
 SearchConduct.propTypes = {
-    titleButton: PropTypes.string.isRequired
+    titleButton: PropTypes.string.isRequired,
+    evento: PropTypes.string.isRequired
+
 }
+
--- src/components/route/TableRoute.js
+++ src/components/route/TableRoute.js
@@ -5,11 +5,13 @@ import * as IoIcons from 'react-icons/io5';
 import * as BsIcons from 'react-icons/bs';
 import * as RiIcons from 'react-icons/ri';
 import * as AiIcons from 'react-icons/ai';
+import * as FcIcons from 'react-icons/fc';
 import { UseModal } from '../../hooks/UseModal';
 import { ModalRoute } from './ModalRoutes';
 import { UseEffectGetRoutes, UseDeleteRoute } from '../../hooks/UseCaseRoute';
 import { Loader } from '../globalComponents/Loader';
 import { SearchConduct } from '../conduct/SearchConduct';
+
 
 export const Route = () => {
 
@@ -17,6 +19,9 @@ export const Route = () => {
     const { data, loading } = UseEffectGetRoutes();
     const [routeData, setRouteData] = useState({});
     const [isEdit, setIsEdit] = useState(false);
+    const [search, setSearch] = React.useState('');
+
+
 
     const handleDeleteRoute = (id_ruta) => {
         console.log(id_ruta)
@@ -37,13 +42,23 @@ export const Route = () => {
                 <h1>Rutas</h1>
                 <span>
                     <SearchConduct titleButton={"Agregar Ruta"} icon={<IoIcons.IoCarSportSharp />} openModal={OpenModalRoute} />
+                    <form className="d-flex navbar-brand" id="formSearch">
+                        <form className="d-flex navbar-brand" id="formSearch">
+                            <input className="form-control me-2" type="text" id="search" placeholder="Buscar" onChange={(evento) => {
+                                setSearch(evento.target.value);
+                            }} />
+                        </form>
+                        <button className="btn btn-outline-success" type="submit"><FcIcons.FcSearch /></button>
+                    </form>
                 </span>
+
+
 
                 <div className="row">
                     <table className="table table-striped table-bordered">
                         <thead>
                             <tr>
-                                
+
                                 <th className="th-shipping" scope="col">Codigo Ruta</th>
                                 <th className="th-shipping" scope="col">Carga</th>
                                 <th className="th-shipping" scope="col">Flete</th>
@@ -55,8 +70,14 @@ export const Route = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {/*loading && <Loader />} */ }
-                            {data.map((route) => (
+                            {/*loading && <Loader />} */}
+                            {data.filter((route) => {
+                                if (search == "") {
+                                    return route
+                                } else if (route.nombre_producto.toLowerCase().includes(search.toLowerCase())) {
+                                    return route
+                                }
+                            }).map((route) => (
                                 <tr key={route.id_ruta}>
                                     <td>{route.id_ruta}</td>
                                     <td>{route.nombre_producto}</td>
@@ -66,7 +87,7 @@ export const Route = () => {
                                     <td>{route.ciudad_destino}</td>
                                     <td>{route.estado}</td>
                                     <td id="columOptions">
-                                    
+
                                         <button className="btn btn-warning btn-sm"><BsIcons.BsFillEyeFill /></button>
                                         <button className="btn btn-info btn-sm" onClick={() => getByIdEdit(route)} ><RiIcons.RiEditFill /></button>
                                         <button className="btn btn-danger btn-sm" onClick={() => handleDeleteRoute(route.id_ruta)} ><AiIcons.AiFillDelete /></button>
