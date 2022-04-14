18dc3b361efb0a7aa3bd9e7f220758a66f4713c9
AngeOro <angelica-om@outlook.com>
2022-04-14 17:31:24 -0500

    Se resuelven conflictos en search conduct

--- src/components/conduct/SearchConduct.js
+++ src/components/conduct/SearchConduct.js
@@ -7,7 +7,6 @@ import '../../Styles/SearchConduct.css';
 import PropTypes from 'prop-types';
 
 
-<<<<<<< HEAD
 export const SearchConduct = ({ titleButton, icon, openModal, setSearch, setCurrentPage, setPage }) => {
 
     const onSearchChange = ({ target }) => {
@@ -16,29 +15,21 @@ export const SearchConduct = ({ titleButton, icon, openModal, setSearch, setCurr
         setSearch(target.value)
     }
 
-=======
->>>>>>> feat/Rutas
 
-
-export const SearchConduct = ({ titleButton, icon, openModal}) => {
-    
-
-    
     return (
         <>
             <nav className="navbar navbar-light bg-light" id="navbarI">
                 <div className="container-fluid">
                     <div className="navbar-brand" id="buttonsActions">
+                        <button type="button" className="btn btn-success"><AiIcons.AiFillFileExcel /></button>
+                        <button type="button" className="btn btn-danger"><AiIcons.AiFillFilePdf /></button>
+                        <button type="button" className="btn btn-info"><ImIcons.ImPrinter /></button>
                         <button type="button" className="btn btn-warning" id="buttonAdd" onClick={openModal}>{icon} {titleButton}</button>
                     </div>
-<<<<<<< HEAD
                     <form className="d-flex navbar-brand" id="formSearch">
                         <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" onChange={onSearchChange} />
                         <button className="btn btn-outline-success" type="submit"><FcIcons.FcSearch /></button>
                     </form>
-=======
-                   
->>>>>>> feat/Rutas
                 </div>
             </nav>
         </>
