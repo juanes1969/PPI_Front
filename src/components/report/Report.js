16768404706265a54b0ac6b972b2f81dc66f3ec8
edwaralej12 <edwaralejandro595@gmail.com>
2022-07-16 14:08:57 -0500

    Create first report functional

--- src/components/report/Report.js
+++ src/components/report/Report.js
@@ -1,111 +1,83 @@
 
 import React, { useState, useEffect } from 'react'
-import {
-  Chart as ChartJS,
-
-  BarElement,
-
-} from 'chart.js';
-
+import { Chart as ChartJS, BarElement,CategoryScale,LinearScale ,Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-
 import { UseEffecReports } from '../../hooks/UseCaseReport';
-// ChartJS.register(
-//   BarElement,
-// );
+
+
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Legend,
+ 
+);
 
 
 export const Report = () => {
 
-  const [reports, setReport] = useState([]);
   const { data } = UseEffecReports();
 
   console.log(data);
+
+  const charReport = {
+    labels: data?.map(x => x.id_vehiculo),
+    datasets: [{
+      label: `${data?.length} Vehiculos con Registros de Rutas`,
+      data: data?.map(x => x.cantidad),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  console.log(charReport)
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    legend: {
+      display: true,
+      labels: {
+        fontColor: "rgb(255, 99, 132)",
+      },
+      position: "bottom",
+      align: "start",
+    },
+  }
+
+  console.log(options)
   return (
-    <div>Report</div>
+    <div>
+      <h1>Report</h1>
+      <Bar
+       data={charReport}
+       height={100}
+    
+      />
+    </div>
   )
 
 
 
 
-
-
-
-
-
-
-  //   var baseUrl = "http://localhost:3000/Report";
-
-
-  // useEffect(() => {
-  //   const fetchCoins = async () => {
-  //     await fetch(`${baseUrl}`, {
-  //       method: 'GET',
-  //       headers: { 
-  //         'Access-Control-Allow-Origin': "*"
-  //       }
-  //     })
-  //       .then((response) => {
-  //         if (response.ok) {
-  //           response.json().then((json) => {
-  //             console.log(json.data);
-  //             setChart(json.data)
-  //           });
-  //         }
-  //       }).catch((error) => {
-  //         console.log(error);
-  //       });
-  //   };
-  //   fetchCoins()
-  // }, [baseUrl])
-
-  console.log("data", data);
-  // var dataa = {
-  //   labels: data.map(x => x.id_vehiculo),
-  //   datasets: [{
-  //     label: `${data.length} `,
-  //     data: data.map(x => x.cantidad),
-  //     backgroundColor: [
-  //       'rgba(255, 99, 132, 0.2)',
-  //       'rgba(54, 162, 235, 0.2)',
-  //       'rgba(255, 206, 86, 0.2)',
-  //       'rgba(75, 192, 192, 0.2)',
-  //       'rgba(153, 102, 255, 0.2)',
-  //       'rgba(255, 159, 64, 0.2)'
-  //     ],
-  //     borderColor: [
-  //       'rgba(255, 99, 132, 1)',
-  //       'rgba(54, 162, 235, 1)',
-  //       'rgba(255, 206, 86, 1)',
-  //       'rgba(75, 192, 192, 1)',
-  //       'rgba(153, 102, 255, 1)',
-  //       'rgba(255, 159, 64, 1)'
-  //     ],
-  //     borderWidth: 1
-  //   }]
-  // };
-
-  // var options = {
-  //   maintainAspectRatio: false,
-  //   scales: {
-  //   },
-  //   legend: {
-  //     labels: {
-  //       fontSize: 25,
-  //     },
-  //   },
-  // }
-
-  // return (
-  //   <div>
-  //     <Bar
-  //       //data={dataa}
-  //       height={400}
-  //       //options={options}
-
-  //     />
-  //   </div>
-  // )
 
 }
 
