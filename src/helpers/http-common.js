45e96167fa9aec0b9495103cdb09a2f3238a27a0
edwaralej12 <edwaralejandro595@gmail.com>
2022-07-22 21:43:01 -0500

    Ajustes expenses

--- src/helpers/http-common.js
+++ src/helpers/http-common.js
@@ -1,5 +1,5 @@
 
 
-export const url_api = `https://sotransiv-app.herokuapp.com/`;
+//export const url_api = `https://sotransiv-app.herokuapp.com/`;
 //export const url_api = `http://localhost:3000/`;
-//export const url_api = `http://localhost:3000/`;
+export const url_api = `http://localhost:3000/`;
