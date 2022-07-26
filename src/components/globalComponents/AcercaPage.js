ebcacdce2b25b65bcd40ea140753383ae9ef2e37
AngeOro <angelica-om@outlook.com>
2022-07-25 20:18:04 -0500

    Se agregan manuales

--- src/components/globalComponents/AcercaPage.js
+++ src/components/globalComponents/AcercaPage.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import usuario from "../../assets/img-cards/ManualUsuario.svg";
 import sistema from "../../assets/img-cards/ManualSistema.svg";
-import { Link } from "react-router-dom";
+import manualUsuario from "../../assets/doc/ManualUsuario.pdf"
+import manualSistema from "../../assets/doc/ManualSistema.pdf"
 
 export const AcercaPage = () => {
   return (
@@ -13,9 +14,9 @@ export const AcercaPage = () => {
             <div class="card-body">
               <h5 class="card-title">Manual de Usuario</h5>
               <p class="card-text">En este manual encontrará las instrucciones y conceptos necesarios para la utilización del software, detallando las diferenctes funciones que ofrece la aplicación. </p>
-              <Link className="btn btn-warning" to="/Conducts">
-                Ver más
-              </Link>
+              <a className="btn btn-warning" href={manualUsuario} download="Manual de Usuario - Sotransiv">
+                Descargar Manual
+              </a>
             </div>
           </div>
         </div>
@@ -25,9 +26,9 @@ export const AcercaPage = () => {
             <div class="card-body">
               <h5 class="card-title">Manual de Sistema </h5>
               <p class="card-text">En este maual encontrará la documentación que se realizó en la etapa de análisis y diseño, para poder iniciar con el desarrollo e implementación de la aplicación. </p>
-              <Link className="btn btn-warning" to="/Vehicles">
-                Ver más
-              </Link>
+              <a className="btn btn-warning" href={manualSistema} download="Manual de Sistema - Sotransiv">
+                Descargar Manual
+              </a>
             </div>
           </div>
         </div>  
